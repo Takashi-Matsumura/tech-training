3e857975564ee18809bb4f2ec826b4d798968d73
Takashi-Matsumura <mats@coral.plala.or.jp>
2024-04-28 16:28:45 +0900

    ブログ投稿一覧の表示を修正しました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
 import Link from "next/link";
+import { PostType } from "./types";
+import { formatDate } from "@/features/utils/myDate";
 
-export default function Home() {
+async function fetchAllBlogs() {
+  const res = await fetch("http://localhost:3000/api/blog", {
+    cache: "no-store", //SSR
+  });
+  const data = await res.json();
+  return data.posts;
+}
+
+export default async function Home() {
+  const posts = await fetchAllBlogs();
+  //console.log(posts);
+
   return (
     <main className="flex flex-col justify-center items-center p-5">
       <h1 className="text-5xl fonnt-bold">Hello</h1>
@@ -13,23 +26,25 @@ export default function Home() {
       <Link href="/blog" className="pb-5">
         Blog投稿
       </Link>
-      <div className="h-screen flex flex-col pb-5">
-        <div className="border p-5 rounded-xl border-blue-800">
-          <div className="flex justify-between">
-            <h1 className="text-2xl font-bold">タイトル</h1>
-            <div className="space-x-5">
-              <button>修正</button>
-              <button>削除</button>
+
+      <div className="w-full h-screen flex flex-col">
+        {posts.map((post: PostType) => (
+          <div key={post.id} className="w-full flex flex-col pb-5">
+            <div className="border p-5 rounded-xl border-gray-500">
+              <div className="flex justify-between">
+                <h1 className="text-2xl font-bold">{post.title}</h1>
+                <div className="space-x-5">
+                  <Link href={`/blog/edit/${post.id}`}>修正</Link>
+                  <Link href={`/blog/delete/${post.id}`}>削除</Link>
+                </div>
+              </div>
+              <h2 className="text-lg">
+                {formatDate(new Date(post.updatedAt))}
+              </h2>
+              <p className="pl-5">{post.content}</p>
             </div>
           </div>
-          <h2 className="text-lg">投稿日</h2>
-          <p className="pl-5">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
-            ullam dolores voluptates quisquam sequi harum corrupti hic optio est
-            eius enim eveniet reiciendis ex, magnam fugiat. Vitae repellat
-            sapiente quibusdam.
-          </p>
-        </div>
+        ))}
       </div>
     </main>
   );
